4f1b2c3c21f65789f930022d0a42ad419a46497a
Saulo93 <saulodlsf@gmail.com>
2017-02-15 13:23:42 +0000

    WindowsformsApplication4 examen de dia 17

---
+++ Segundo trimestre/WindowsFormsApplication4/WindowsFormsApplication4/Registro.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication4
+{
+   
+        enum tipo
+        {
+            cadena = 1,
+            entero = 2
+        }
+
+        class Registro
+        {
+            List<string> campos;
+            List<tipo> tipos;
+            int numCampos;
+
+            public Registro(List<string> lista1, List<tipo> lista2)
+            {
+                int i = 0;
+                campos = new List<string>();
+                tipos = new List<tipo>();
+                for (i = 0; i < lista1.Count; i++)
+                {
+                    campos.InsertRange(0, lista1);
+                    tipos.InsertRange(0, lista2);
+
+                }
+                numCampos = lista1.Count;
+
+            }
+
+            public void escribe(List<string> listavalores, BinaryWriter bw)
+            {
+                int entero = 0;
+
+                try
+                {
+                    for (int i = 0; i < listavalores.Count; i++)
+                    {
+                        switch (tipos[i])
+                        {
+                            case tipo.cadena:
+                                bw.Write(listavalores[i].ToString());
+                                break;
+                            case tipo.entero:
+                                Int32.TryParse(listavalores[i], out entero);
+                                bw.Write(entero);
+                                break;
+                            default:
+                                throw new Exception("Tipo de dato no permitido");
+                        }
+                    }
+                }
+                catch (Exception e1)
+                {
+                    throw new Exception(e1.Message + "\n" + e1.Source);
+                }
+            }
+
+
+            public List<string> lee(BinaryReader br)
+            {
+                List<string> devol;
+                devol = new List<string>();
+                try
+                {
+                    for (int i = 0; i < numCampos; i++)
+                    {
+                        switch (tipos[i])
+                        {
+                            case tipo.cadena:
+                                devol.Add(br.ReadString());
+                                break;
+                            case tipo.entero:
+                                devol.Add(br.ReadInt32().ToString());
+                                break;
+                            default:
+                                throw new Exception("Tipo de dato no permitido");
+                        }
+                    }
+                }
+                catch (Exception e1)
+                {
+                    throw new Exception(e1.Message + "\n" + e1.Source);
+                }
+                return (devol);
+            }
+        }
+    }
+
+
