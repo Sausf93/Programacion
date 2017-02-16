9cc81f3d6f5ee0ff69fb8ae9ba330ac238429e7e
Saulo93 <saulodlsf@gmail.com>
2017-02-16 20:28:35 +0000

    Liga de futbol modificada con order y botones en enable a false

---
+++ Segundo trimestre/liga de futbol terminada/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Numero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Victorias = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Empates = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Derrotas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Puntos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(666, 399);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox5);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(658, 373);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(519, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 68);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Graba";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonGraba_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(519, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 66);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Crear fichero";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonCrea_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(242, 312);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(242, 235);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(242, 166);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(242, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(215, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(242, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(38, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Derrotas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Empates";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Victorias";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nombre de Equipo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Numero";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(658, 373);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(262, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 55);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "escribir datagrip";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.buttoncargaNedias_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Numero,
+            this.Nombre,
+            this.Victorias,
+            this.Empates,
+            this.Derrotas,
+            this.Puntos});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(645, 250);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Numero
+            // 
+            this.Numero.HeaderText = "Numero";
+            this.Numero.Name = "Numero";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Victorias
+            // 
+            this.Victorias.HeaderText = "Victorias";
+            this.Victorias.Name = "Victorias";
+            // 
+            // Empates
+            // 
+            this.Empates.HeaderText = "Empates";
+            this.Empates.Name = "Empates";
+            // 
+            // Derrotas
+            // 
+            this.Derrotas.HeaderText = "Derrotas";
+            this.Derrotas.Name = "Derrotas";
+            // 
+            // Puntos
+            // 
+            this.Puntos.HeaderText = "Puntos";
+            this.Puntos.Name = "Puntos";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(658, 373);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(171, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(298, 172);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Calcula puntos";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 393);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Numero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Victorias;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Empates;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Derrotas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Puntos;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Segundo trimestre/liga de futbol terminada/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool pulsar;
+        Fichero f1, f2;
+        List<string> l1;
+        List<tipo> l2;
+        List<string> cajas;
+
+
+
+        private void buttonGraba_Click(object sender, EventArgs e)
+        {
+            if (pulsar)
+            {
+                try
+                {
+                    if (f1.abre())
+                    {
+                        f1.fin();
+                        carga(cajas);
+                        f1.escribe(cajas);
+                    }
+                }
+                catch (Exception e1)
+                {
+                    MessageBox.Show(e1.Message);
+                }
+                finally
+                {
+                    f1.cierra();
+                }
+            } 
+        }
+
+        private void buttonCrea_Click(object sender, EventArgs e)
+        {
+            pulsar = true;
+            button4.Enabled = true;
+            button3.Enabled = true;
+            button2.Enabled = true;
+            l1 = new List<string>();
+            l2 = new List<tipo>();
+            cajas = new List<string>();
+            l1.Add("numero");
+            l1.Add("nombre");
+            l1.Add("Victorias");
+            l1.Add("Empates");
+            l1.Add("Derrotas");
+
+            l2.Add(tipo.entero);
+            l2.Add(tipo.cadena);
+            l2.Add(tipo.entero);
+            l2.Add(tipo.entero);
+            l2.Add(tipo.entero);
+            try
+            {
+                f1 = new Fichero("Equipos.dat", l1, l2);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+                return;
+            }
+            l1.Clear();
+            l2.Clear();
+            l1.Add("numero");
+            l1.Add("nombre");
+            l1.Add("victoria");
+            l1.Add("empates");
+            l1.Add("derrotas");
+            l1.Add("puntos");
+
+            l2.Add(tipo.entero);
+            l2.Add(tipo.cadena);
+            l2.Add(tipo.entero);
+            l2.Add(tipo.entero);
+            l2.Add(tipo.entero);
+            l2.Add(tipo.entero);
+            try
+            {
+                f2 = new Fichero("Resultados.dat", l1, l2);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+                return;
+            }
+        }
+
+        private void buttonCalculaMedia_Click(object sender, EventArgs e)
+        {
+            if (pulsar)
+            {
+                List<string> caja2;
+                f1.abre();
+                f2.trunca();
+                f2.abre();
+                cajas.Clear();
+                caja2 = new List<string>();
+                int victorias, empates, puntos;
+                for (int i = 0; i < f1.numRegistros; i++)
+                {
+                    caja2.Clear();
+                    try
+                    {
+
+                        cajas = f1.lee();
+                        victorias = Convert.ToInt32(cajas[2]);
+                        empates = Convert.ToInt32(cajas[3]);
+                        puntos = (victorias * 3) + empates;
+                        caja2.Add(cajas[0]);
+                        caja2.Add(cajas[1]);
+                        caja2.Add(cajas[2]);
+                        caja2.Add(cajas[3]);
+                        caja2.Add(cajas[4]);
+                        caja2.Add(puntos.ToString());
+                        try
+                        {
+                            f2.escribe(caja2);
+                        }
+                        catch (Exception e1)
+                        {
+                            MessageBox.Show(e1.Message + "\n" + e1.Source);
+                        }
+                    }
+                    catch (Exception e1)
+                    {
+                        MessageBox.Show(e1.Message);
+                    }
+                }
+                f1.cierra();
+                f2.cierra();
+            } 
+        }
+
+        private void buttoncargaNedias_Click(object sender, EventArgs e)
+        {
+            if (pulsar)
+            {
+                int i = 0;
+                List<string> caja3;
+                caja3 = new List<string>();
+                f2.abre();
+                dataGridView1.Rows.Clear();
+                for (i = 0; i < f2.numRegistros; i++)
+                {
+                    caja3.Clear();
+                    try
+                    {
+                        caja3 = f2.lee();
+                        dataGridView1.Rows.Add(caja3[0], caja3[1], caja3[2], caja3[3], caja3[4], caja3[5]);
+                        dataGridView1.Sort(dataGridView1.Columns[5], ListSortDirection.Descending);
+                    }
+                    catch (Exception e1)
+                    {
+                        MessageBox.Show(e1.Message);
+                    }
+                }
+                f2.cierra();
+            } 
+        }
+
+        void carga(List<string> cajas)
+        {
+            cajas.Clear();
+            cajas.Add(textBox1.Text);
+            cajas.Add(textBox2.Text);
+            cajas.Add(textBox3.Text);
+            cajas.Add(textBox4.Text);
+            cajas.Add(textBox5.Text);
+        }
+    }
+}
+
